9083fda6b18e13f501bcd58dfc4c5d8c828b599d
Dimas Neto <dimas_fcn@outlook.com>
2022-04-26 22:47:41 -0300

    finish

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -13,9 +13,11 @@ export const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     const recoveredUsed = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
 
-    if (recoveredUsed) {
+    if (recoveredUsed && token) {
       setUser(JSON.parse(recoveredUsed));
+      api.defaults.headers.Authorization = `Bearer ${token}`;
     }
 
     setLoading(false);
@@ -30,17 +32,22 @@ export const AuthProvider = ({ children }) => {
     const token = response.data.token;
 
     localStorage.setItem("user", JSON.stringify(loggedUser));
-    localStorage.setItem("token", JSON.stringify(token));
+    localStorage.setItem("token", token);
 
-    if (passord === "secret") {
-      setUser(loggedUser);
-      navigate("/");
-    }
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    setUser(loggedUser);
+    navigate("/");
   };
 
   const logout = () => {
     console.log("logout");
+
     localStorage.removeItem("user");
+    localStorage.removeItem("token");
+
+    api.defaults.headers.Authorization = null;
+
     setUser(null);
     navigate("/login");
   };
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,18 +1,41 @@
-import React, { useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 
 import { AuthContext } from "../../contexts/auth";
 
+import { getUsers } from "../../services/api";
+
 const HomePage = () => {
-  const { authenticated, logout } = useContext(AuthContext);
+  const { logout } = useContext(AuthContext);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const response = await getUsers();
+      setUsers(response.data);
+      setLoading(false);
+    })();
+  }, []);
+
   const handleLogout = () => {
     logout();
   };
 
+  if (loading) {
+    return <div className="loading">Carregando dados...</div>;
+  }
+
   return (
     <>
       <h1>HomePage</h1>
-      <p>{String(authenticated)}</p>
       <button onClick={handleLogout}>Logout</button>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.id} - {user.email}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
